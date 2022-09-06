8b305371ad40b785789f0b26658881e75a3f61ad
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-06 13:36:56 -0300

    a012_exercicio02

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt('Forneça a idade da pessoa:'))
+let isMaiorIdade = Boolean(prompt('A pessoa já terminou o ensino médio?'))
+let isCursandoOutraFaculdade = Boolean(prompt('A pessoa está cursando outra faculdade?'))
 
-if(){
-
+if(idade >= 18){
+    console.log('A pessoa é maior de idade.')
 }else{
-	
+	console.log('A pessoa é menor de idade.')
+}
+
+if(isMaiorIdade){
+    console.log('A pessoa terminou o ensino médio.')
+}else{
+    console.log('A pessoa não terminou o ensino médio.')
+}
+
+if(isCursandoOutraFaculdade){
+    console.log('A pessoa está cursando outra faculdade.')
+}else{
+    console.log('A pessoa não está cursando outra faculdade.')
 }
