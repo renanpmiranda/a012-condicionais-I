b3be8d18aef7079a345dd2ee4498aa28c2716b41
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-06 13:19:24 -0300

    a012_exercicio01

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = Number(prompt('Forneça um número:'))
 
-if(){
-    console.log("sua mensagem aqui");
+if(num % 2 === 0){
+    console.log('o número é par')
+}else{
+    console.log('o número é ímpar')
 }
